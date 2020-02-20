b2204b96cb01c55562b276abee8f5e92584ce612
wuliwux <christian.mueller@xcore.at>
2020-02-20 15:13:20 +0100

    init

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"eta-rs232 adapter settings": {
+		en: "Adapter settings for eta-rs232",
+		de: "Adaptereinstellungen für eta-rs232",
+		ru: "Настройки адаптера для eta-rs232",
+		pt: "Configurações do adaptador para eta-rs232",
+		nl: "Adapterinstellingen voor eta-rs232",
+		fr: "Paramètres d'adaptateur pour eta-rs232",
+		it: "Impostazioni dell'adattatore per eta-rs232",
+		es: "Ajustes del adaptador para eta-rs232",
+		pl: "Ustawienia adaptera dla eta-rs232",
+		"zh-cn": "eta-rs232的适配器设置",
+	},
+	option1: {
+		en: "option1",
+		de: "Option 1",
+		ru: "Опция 1",
+		pt: "Opção 1",
+		nl: "Optie 1",
+		fr: "Option 1",
+		it: "opzione 1",
+		es: "Opción 1",
+		pl: "opcja 1",
+		"zh-cn": "选项1",
+	},
+	option2: {
+		en: "option2",
+		de: "Option 2",
+		ru: "option2",
+		pt: "opção 2",
+		nl: "Optie 2",
+		fr: "Option 2",
+		it: "opzione 2",
+		es: "opcion 2",
+		pl: "Opcja 2",
+		"zh-cn": "选项2",
+	},
+};
